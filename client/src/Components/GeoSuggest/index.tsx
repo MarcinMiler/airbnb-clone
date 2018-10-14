9d693747e272379218155e6ebb28f0e97267130c
Marcin Miler <marcin4951@gmail.com>
2018-10-14 13:15:15 +0200

    Geosuggest input

---
+++ client/src/Components/GeoSuggest/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { FieldProps } from 'formik'
+import Geosuggest, { Suggest } from 'react-geosuggest'
+
+import './geoInput.css'
+
+export class GeoSuggest extends React.PureComponent<FieldProps<any>> {
+    private onSuggestSelect = (place: Suggest) => {
+        if (!place) {
+            return
+        }
+
+        const {
+            location: { lat, lng }
+        } = place
+
+        const {
+            form: { setValues, values }
+        } = this.props
+
+        setValues({
+            ...values,
+            lat,
+            lng
+        })
+    }
+
+    public render() {
+        return (
+            <Geosuggest
+                placeholder="Enter a location"
+                onSuggestSelect={this.onSuggestSelect}
+                location={new google.maps.LatLng(53.558572, 9.9278215)}
+                radius={20}
+            />
+        )
+    }
+}
--- client/src/Pages/CreateListing/Components/LocationForm/index.tsx
+++ client/src/Pages/CreateListing/Components/LocationForm/index.tsx
@@ -1,27 +1,13 @@
 import * as React from 'react'
 import { Field } from 'formik'
 
-import { FormikInput } from 'src/Components/FomikInput'
+import { GeoSuggest } from 'src/Components/GeoSuggest'
 import { Title } from './style'
 
 export const LocationForm: React.SFC<{}> = () => (
     <>
         <Title>Location</Title>
 
-        <Field
-            name="lat"
-            placeholder="Lat"
-            label="Lat"
-            type="number"
-            component={FormikInput}
-        />
-
-        <Field
-            name="lng"
-            placeholder="Lng"
-            label="Lng"
-            type="number"
-            component={FormikInput}
-        />
+        <Field component={GeoSuggest} />
     </>
 )
